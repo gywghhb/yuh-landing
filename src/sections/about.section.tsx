77ade53689d690c4bf890fdd05a99175a284b956
Tom Guo <gywghhb@gmail.com>
2023-09-03 12:19:34 -0400

    update about section text width

--- src/sections/about.section.tsx
+++ src/sections/about.section.tsx
@@ -9,7 +9,7 @@ interface Props {}
 const AboutSection: FC<Props> = () => {
   const titleStyle = ndot47.className + " text-4xl mb-10 max-w-[460px]"
   const subtitleStyle = ndot47.className + " text-[14px] text-[#C3C3C3] mb-1"
-  const pStyle = "text-[#A7A7A7] mb-4 max-w-[600px]"
+  const pStyle = "text-[#A7A7A7] mb-4"
 
   return (
     <section id="about">
@@ -21,7 +21,7 @@ const AboutSection: FC<Props> = () => {
             <p className={pStyle}>We are bringing hundreds of developers, designers, and ideators to explore what&apos;s possible by working together in a 2-day, 24-hour hackathon.</p>
             <p className={pStyle}>Whether you&apos;re an experienced hacker or someone who&apos;s learning the rope, we have a place for your and we&apos;re eager see what inspiring project you&apos;ll bring to the presentation booth.</p>
           </div>
-          <div id="about-media" className="mx-6 my-12 md:mr-14 md:my-14">
+          <div id="about-media" className="flex shrink-0 mx-6 my-12 md:mr-14 md:my-14">
             MEDIA
           </div>
         </SectionBackground>
