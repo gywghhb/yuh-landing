9e9523b8afab3cbc6292aa8ac048d43909351b8a
Tom Guo <gywghhb@gmail.com>
2023-09-03 07:03:10 -0400

    add event card

--- src/components/expandable-card.component.tsx
+++ src/components/expandable-card.component.tsx
@@ -1,15 +1,26 @@
 import { FC } from "react";
 
 interface Props {
-  id?: string | null,
+  id?: string | undefined,
   children?: React.ReactNode | null,
   className?: string | null,
 }
-
-const ExpandableCard: FC<Props> = ({id, children}) => {
+/**
+ * Expandable Card
+ * ====================
+ * 
+ * 
+ * @param id the id of the background div (usually not needed).
+ * @param children HTML elements to render inside the div.
+ * @param className class and additional Tailwind style of the background div.
+ */
+const ExpandableCard: FC<Props> = ({id, children, className}) => {
 
   return (
-    <div>
+    <div
+      id={id}
+      className={`shadow-lg bg-[#FDFDFD] p-10 rounded-2xl ${className}`}
+    >
       {children}
     </div>
   )
--- src/sections/events.section.tsx
+++ src/sections/events.section.tsx
@@ -7,6 +7,34 @@ import ExpandableCard from "@/components/expandable-card.component";
 
 interface Props {}
 
+interface EventCardProps {
+  title: string;
+  titleIcon?: React.ReactNode | null;
+  description: string;
+  body?: React.ReactNode | null;
+  className?: string | null;
+}
+
+const EventCard: FC<EventCardProps> = ({
+  title,
+  titleIcon,
+  description,
+  body,
+  className,
+}) => {
+  return (
+    <ExpandableCard className={className}>
+      <div className="flex flex-col">
+        <div className="flex shrink-0">
+          {titleIcon}
+        </div>
+        <h5 className="">{title}</h5>
+      </div>
+      <p>{description}</p>
+    </ExpandableCard>
+  );
+};
+
 const EventSection: FC<Props> = () => {
   const titleStyle = ndot47.className + " text-4xl mb-10 max-w-[560px]";
   const subtitleStyle = ndot47.className + " text-[14px] text-[#C3C3C3] mb-1";
@@ -18,7 +46,7 @@ const EventSection: FC<Props> = () => {
         <SectionBackground className="flex-col">
           <div
             id="event-text-container"
-            className="mx-6 my-12 md:ml-14 md:my-14"
+            className="mx-6 mt-12 md:mx-14 md:mt-14"
           >
             <span className={subtitleStyle}>&gt; EVENT</span>
             <h1 className={titleStyle}>
@@ -32,10 +60,16 @@ const EventSection: FC<Props> = () => {
               to make it happens.
             </p>
           </div>
-          <div id="event-details-container">
+          <div
+            id="event-details-container"
+            className="mx-6 mb-12 mt-8 md:mx-14 md:mb-14"
+          >
             {/* TODO */}
             <div>
-              <ExpandableCard></ExpandableCard>
+              <EventCard 
+                title="Title"
+                description="This is a description."
+              />
             </div>
           </div>
         </SectionBackground>
