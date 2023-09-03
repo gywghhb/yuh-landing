c0af7b298e9fa73bd382f9c8f1150bb7604369b8
Tom Guo <gywghhb@gmail.com>
2023-09-02 23:39:48 -0400

    fix typo in footer

--- src/sections/footer.section.tsx
+++ src/sections/footer.section.tsx
@@ -14,9 +14,9 @@ const Footer: FC<Props> = () => {
         id="footer-background"
         className="flex flex-col md:flex-row bg-[#303030] w-full mx-4 px-6 pt-8 pb-6 text-[#767676] rounded-t-3xl"
       >
-        <div id="footer-title" className="text-[12px]">yuHacks brought to you with pride by: </div>
+        <div id="footer-title" className="text-xs">yuHacks brought to you with pride by: </div>
         <div 
-          id="footer carousel"
+          id="footer-carousel"
           className=""
         >
           Something
